d73b5415cc809005d31ff68119ac1e69df8facb0
Eniolayo <eniolayodeji@gmail.com>
2023-05-25 17:10:45 +0100

    fix

--- src/components/Home/Ads.js
+++ src/components/Home/Ads.js
@@ -1,5 +1,7 @@
-import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import { Carousel } from "react-responsive-carousel";
+
 import { Heading, Text } from "../ui";
 export default function Ads() {
   return (
--- src/components/services/benefit.js
+++ src/components/services/benefit.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Heading, Text } from "../ui";
 
 export default function Benefit({ services = [] }) {
